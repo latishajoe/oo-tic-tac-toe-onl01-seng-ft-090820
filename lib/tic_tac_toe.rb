71ca4580ca6bdfb7e621277e32d7fa3f45c44f63
Latisha Joe <latisha_joe@yahoo.com>
2020-09-22 01:02:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,18 +92,18 @@ class TicTacToe
         WIN_COMBINATIONS.detect do |win_combo|
           if (@board[win_combo[0]]) == "X" &&
             (@board[win_combo[1]]) == "X" &&
-            (@board[win_combo[2]]) == "X" 
+            (@board[win_combo[2]]) == "X"
              return "X"
           elsif  (@board[win_combo[0]]) == "O" &&
              (@board[win_combo[1]]) == "O" &&
-             (@board[win_combo[2]]) == "O" 
+             (@board[win_combo[2]]) == "O"
               return "O"
-          else 
-            nil 
-          end 
-        end 
-      end 
-    end      
+          else
+            nil
+          end
+        end
+      end
+    end
 
 
 
